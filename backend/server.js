272f1b03c6c2c87c00930782c5de97ce28f319b0
Rohitrout416 <rohitiitrout416@gmail.com>
2024-10-09 23:21:42 +0530

    added frontend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import './Models/db.js';
+import AuthRouter from './Routes/AuthRouter.js';
+import ProductRouter from './Routes/ProductRouter.js';
+
+const app = express();
+dotenv.config();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.use(express.json());
+app.use(cors());
+app.use('/auth', AuthRouter);
+app.use('/products', ProductRouter);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+})
